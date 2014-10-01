b39523f8d67f50d0b614577c11951cd221a10b28
rfkdali <rfkdali@gmail.com>
2014-10-01 11:58:19 +0200

    ajout conditions elif pour test 5 donne Buzz

--- test_fizz_buzz.py
+++ test_fizz_buzz.py
@@ -5,9 +5,12 @@ class TestFizzBuzz(unittest.TestCase):
 	def test_3_donne_fizz(self):
 		self.assertEqual("Fizz", fizz_buzz(3))
 	def test_5_donne_buzz(self):
-		self.assertEqual("Buzz", fizz_buzz(3))
+		self.assertEqual("Buzz", fizz_buzz(5))
 def fizz_buzz(chiffre):
-	return chiffre
+	if chiffre == 3:
+		return "Fizz"
+	elif chiffre == 5:
+		return "Buzz"
 
 if __name__ == '__main__':
 	unittest.main()
