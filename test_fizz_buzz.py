1c752c73d4d5dd51f4804bd77c7f67e24bdeeba4
rfkdali <rfkdali@gmail.com>
2014-10-01 12:01:34 +0200

    ajout condition test 15 donne fizzbuzz

--- test_fizz_buzz.py
+++ test_fizz_buzz.py
@@ -6,11 +6,15 @@ class TestFizzBuzz(unittest.TestCase):
 		self.assertEqual("Fizz", fizz_buzz(3))
 	def test_5_donne_buzz(self):
 		self.assertEqual("Buzz", fizz_buzz(5))
+	def test_15_donne_buzz(self):
+		self.assertEqual("FizzBuzz", fizz_buzz(15))
 def fizz_buzz(chiffre):
 	if chiffre == 3:
 		return "Fizz"
 	elif chiffre == 5:
 		return "Buzz"
+	elif chiffre == 15:
+		return "FizzBuzz"
 
 if __name__ == '__main__':
 	unittest.main()
