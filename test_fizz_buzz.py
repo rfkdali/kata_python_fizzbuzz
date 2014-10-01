8d6c6ec5bf85250289a94775b2cbb15229c94e0f
rfkdali <rfkdali@gmail.com>
2014-10-01 11:43:28 +0200

    test

---
+++ test_fizz_buzz.py
@@ -0,0 +1,10 @@
+import unittest
+
+class TestFizzBuzz(unittest.TestCase):
+
+	def test_test(self):
+		self.assertEqual("Fizz", fizz_buzz(3))
+def fizz_buzz(chiffre):
+	return "Fizz"
+if __name__ == '__main__':
+	unittest.main()
