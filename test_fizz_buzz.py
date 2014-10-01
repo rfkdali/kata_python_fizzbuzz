457e9de33b8dbe8c4587c2b12443652e6e9eff53
rfkdali <rfkdali@gmail.com>
2014-10-01 11:49:13 +0200

    ajout test 5 donne Buzz

--- test_fizz_buzz.py
+++ test_fizz_buzz.py
@@ -2,9 +2,13 @@ import unittest
 
 class TestFizzBuzz(unittest.TestCase):
 
-	def test_test(self):
+	def test_3_donne_fizz(self):
 		self.assertEqual("Fizz", fizz_buzz(3))
-def fizz_buzz(chiffre):
-	return "Fizz"
+	def test_5_donne_buzz(self):
+		self.assertEqual("Buzz", fizz_buzz(3))
+	def fizz_buzz(chiffre):
+		return "Fizz"
+
 if __name__ == '__main__':
 	unittest.main()
+ 
